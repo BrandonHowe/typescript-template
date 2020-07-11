5bef67a7b44fe71a90d389b85b7e9e64468e68d7
xWafl <73sampleperson@gmail.com>
2020-07-11 07:31:06 -0400

    delete: deleted a bunch of stuff

--- src/modules/apiRouter.ts
+++
@@ -1,9 +0,0 @@
-import Router from "koa-router";
-
-import greetingsRouter from "./greetings/routes/";
-
-const apiRouter = new Router({ prefix: "/api" });
-
-apiRouter.use(greetingsRouter);
-
-export default apiRouter.routes();
--- src/modules/greetings/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import Router from "koa-router";
-
-import root from "./root";
-
-const greetingsRouter = new Router({ prefix: "/greetings" });
-
-greetingsRouter.use(root);
-
-export default greetingsRouter.routes();
